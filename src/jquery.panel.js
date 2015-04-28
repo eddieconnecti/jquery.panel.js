9c0e492645cddc508b0efbcf106520c30ef4a89d
eddieconnecti <eddieconnecti@users.noreply.github.com>
2015-04-28 14:00:38 +0200

    Update jquery.panel.js

--- src/jquery.panel.js
+++ src/jquery.panel.js
@@ -106,7 +106,7 @@
 	 * </table>
 	 *
 	 * jQuery Panel
-	 * Copyright 2015 K. Feldmaier (eddieconnecti@googlemail.com)
+	 * Copyright 2015 Kai Feldmaier (kai.feldmaier@gmail.com)
 	 * Licensed under MIT (http://opensource.org/licenses/MIT)
 	 */
 ( function( $ ) {
